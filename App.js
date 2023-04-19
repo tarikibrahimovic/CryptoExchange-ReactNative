46df2f515ddce1ffef415ebe15a7508f8cb5744a
Tarik Ibrahimovic <tarikibrahimovic2016@gmail.com>
2023-04-19 20:56:58 +0200

    login page done

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+import LoginScreen from './screens/LoginScreen';
+import styled from 'styled-components';
+
+const Container = styled.View`
+  flex: 1;
+  // background-color: #3c4564;
+  background-color: #1F2630;
+  // background-color: #697597;  
+  justify-content: center;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <LoginScreen/>
+      <StatusBar style="light" />
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/LoginScreen.js
@@ -0,0 +1,167 @@
+import React from "react";
+import { useState } from "react";
+import { View, SafeAreaView, ScrollView } from "react-native";
+import { Button } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+import logo from "../assets/logo.js";
+import styled from "styled-components";
+import { TextInput } from "react-native-paper";
+
+const CustomSafeArea = styled(SafeAreaView)`
+  flex: 1;
+`;
+
+const InputSection = styled.View`
+  margin: 0 10px;
+  margin-top: 20px;
+  padding: 10px;
+  background-color: #29313C;
+  border-radius: 10px;
+  gap: 15px;
+`;
+
+const LogoSection = styled.View`
+  margin-bottom: 30px;
+  //   padding: 10px;
+  //   border-radius: 10px;
+  //   background-color: #697597;
+  justify-content: center;
+  align-items: center;
+  font-family: "Roboto";
+`;
+
+const LogoText = styled.Text`
+  font-size: 32px;
+  color: white;
+  font-weight: bold;
+  font-family: "Roboto";
+`;
+
+const Label = styled.Text`
+  font-size: 16px;
+  color: white;
+  font-weight: bold;
+  font-family: "Roboto";
+`;
+
+const Line = styled.View`
+  height: 1px;
+  width: 100%;
+  background-color: #fff;
+  margin-bottom: 5px;
+`;
+
+const LoginText = styled.Text`
+  font-size: 24px;
+  color: white;
+  font-weight: bold;
+  text-align: center;
+  margin: 5px 0;
+  font-family: "Roboto";
+`;
+
+const ForgotPassword = styled.Text`
+  font-size: 16px;
+  color: #3837fd;
+`;
+
+const ErrorText = styled.Text`
+    font-size: 16px;
+    color: red;
+    font-weight: bold;
+    text-align: center;
+    margin: 5px 0;
+    font-family: "Roboto";
+    `;
+
+const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+  
+  const validatePassword = (password) => {
+    return password.length >= 8;
+  };
+  
+
+
+
+export default function LoginScreen() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [errors, setErrors] = useState("");
+
+  const handleLogin = () => {
+    console.log("pokrenulo se");
+    if (!validateEmail(email)) {
+        setErrors("invalid email");
+    } else if (!validatePassword(password)) {
+        setErrors("invalid password");
+    } else {
+      // handle successful login
+      console.log("success");
+    }
+  };
+
+  return (
+    <CustomSafeArea>
+      <ScrollView>
+        <LogoSection>
+          <SvgXml xml={logo} width="400" height="300" color="white" />
+          <LogoText>Crypto Exchange</LogoText>
+        </LogoSection>
+        <InputSection>
+          <LoginText>Login</LoginText>
+          <Line></Line>
+          {/* if there is an error, show it here */}
+            {errors ? <ErrorText>{errors}</ErrorText> : null}
+          <View>
+            <Label>Email</Label>
+            <TextInput
+              value={email}
+              outlineColor="#3658C8"
+              mode="outlined"
+              onChangeText={(text) => setEmail(text)}
+              placeholder="email@gmail.com"
+              required
+            />
+          </View>
+          <View>
+            <Label>Password</Label>
+            <TextInput
+              mode="outlined"
+              placeholder="*******"
+              secureTextEntry={!showPassword}
+              right={
+                <TextInput.Icon
+                  icon="eye"
+                  onPress={() => setShowPassword(!showPassword)}
+                />
+              }
+              onChangeText={(text) => setPassword(text)}
+            />
+          </View>
+          <View>
+            <Button
+              mode="contained"
+              onPress={() => handleLogin()}
+              buttonColor="#FCD434"
+              textColor="#1F2630"
+            >
+              Login
+            </Button>
+          </View>
+          <View>
+            <ForgotPassword>Forgot Password?</ForgotPassword>
+          </View>
+          <Line />
+          <View>
+            <ForgotPassword>Don't have an account? Sign Up!</ForgotPassword>
+          </View>
+        </InputSection>
+      </ScrollView>
+    </CustomSafeArea>
+  );
+}
